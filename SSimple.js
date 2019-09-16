ad01ac274cd1ff80976fd66c9c083656e4a6067a
john-warner <warnerkenpo@gmail.com>
2019-09-15 21:05:50 -0600

    Name changes

--- SSimple.js
+++ SSimple.js
@@ -30,11 +30,11 @@ var $$ = function() {
         return parms.length > 0 && parms[0] == 'e';
     }
   
-    var sugar = {};
+    var exports = {};
 
     // Object manipulation
 
-    sugar.extend = (target, source) => Object.assign(target, source);
+    exports.extend = (target, source) => Object.assign(target, source);
 
     // Function composition
 
@@ -42,59 +42,60 @@ var $$ = function() {
     const backflow = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
     const curry = fn => (...args) => fn.bind(null, ...args);
     const element = (e) => (typeof e === 'string') ? document.querySelector(e) : e;
+    const find = (e, sel) => (typeof e !== 'string') ? e.querySelector(sel) : document.querySelector(e); // use first parameteer as sel
 
-    sugar.oneach = (a, f) => a.foreach(f);
-    sugar.curry = curry;
-    sugar.map = curry((fn, arr) => arr.map(fn));
-    sugar.join = curry((str, arr) => arr.join(str));
-    sugar.toLowerCase = str => str.toLowerCase();
-    sugar.split = curry((splitOn, str) => str.split(splitOn));
+    exports.oneach = (a, f) => a.foreach(f);
+    exports.curry = curry;
+    exports.map = curry((fn, arr) => arr.map(fn));
+    exports.join = curry((str, arr) => arr.join(str));
+    exports.toLowerCase = str => str.toLowerCase();
+    exports.split = curry((splitOn, str) => str.split(splitOn));
 
-    sugar.flow = flow;
-    sugar.backflow = backflow;
-    sugar.compose = backflow;
+    exports.flow = flow;
+    exports.backflow = backflow;
+    exports.compose = backflow;
 
-    sugar.isFunction = (f) => typeof f === 'function';
-    sugar.isPlainObject = (o) => Object.prototype.toString.call(o) === '[object Object]';
-    sugar.isArray = (a) => Array.isArray(a);
-    sugar.isString = (s) => typeof s === 'string';
+    exports.isFunction = (f) => typeof f === 'function';
+    exports.isPlainObject = (o) => Object.prototype.toString.call(o) === '[object Object]';
+    exports.isArray = (a) => Array.isArray(a);
+    exports.isString = (s) => typeof s === 'string';
 
     // DOM manipulation
 
-    sugar.select = document.querySelector.bind(document);
-    sugar.find = (e, sel) => (typeof e !== 'string') ? e.querySelector(sel) : document.querySelector(e); // use first parameteer as sel
-    sugar.findall = (e, sel) => (typeof e !== 'string') ? e.querySelectorAll(sel) : document.querySelectorAll(e);
-    sugar.findid = (e, id) => (typeof e !== 'string') ? e.getElementById(id) : document.getElementById(e);
-    sugar.findname = (e, name) => (typeof e !== 'string') ? e.getElementsByName(name) : document.getElementsByName(e);
-    sugar.findtag = (e, tag) => (typeof e !== 'string') ? e.getElementsByTagName(tag) : document.getElementsByTagName(e);
+    exports.select = document.querySelector.bind(document);
+    exports.find = find;
+    exports.findall = (e, sel) => (typeof e !== 'string') ? e.querySelectorAll(sel) : document.querySelectorAll(e);
+    exports.findid = (e, id) => (typeof e !== 'string') ? e.getElementById(id) : document.getElementById(e);
+    exports.findname = (e, name) => (typeof e !== 'string') ? e.getElementsByName(name) : document.getElementsByName(e);
+    exports.findtag = (e, tag) => (typeof e !== 'string') ? e.getElementsByTagName(tag) : document.getElementsByTagName(e);
 
-    sugar.clone = (e) => element(e).cloneNode(true);
-    sugar.create = (tag) => document.createElement(tag);
-    sugar.template = (t,sel) => element(t).content.querySelector(sel).cloneNode(true); 
+    exports.clone = (e) => element(e).cloneNode(true);
+    exports.create = (tag) => document.createElement(tag);
+    exports.template = (t,sel) => element(t).content.querySelector(sel).cloneNode(true); 
 
-    sugar.contains = (e, child) => e != child && e.contains(child);
-    sugar.empty = (e) => e.innerHTML = '';
-    sugar.html = (e, outer) => (outer) ? e.outerHTML : e.innerHTML;
-    sugar.text = (e,v) => (typeof v === 'undefined') ? e.textContent : e.textContent = v;
-    sugar.remove = (e) => e.parentNode.removeChild(e);
-    sugar.append = (e, c) => e.appendChild(c);
+    exports.contains = (e, child) => e != child && e.contains(child);
+    exports.empty = (e) => e.innerHTML = '';
+    exports.html = (e, outer) => (outer) ? e.outerHTML : e.innerHTML;
+    exports.text = (e,v) => (typeof v === 'undefined') ? e.textContent : e.textContent = v;
+    exports.remove = (e) => e.parentNode.removeChild(e);
+    exports.append = (e, c) => e.appendChild(c);
 
-    sugar.hasclass = (e, name) => e.classList.contains(name);
-    sugar.css = (e,p) => getComputedStyle(e,p);
-    sugar.cssrule = (e, rule) => getComputedStyle(e)[rule];
-    sugar.cssToggle = (e, rule) => e.classList.toggle(rule);
-    sugar.cssReplace = (e, rule, newRule) => e.classList.replace(rule, newRule);
-    sugar.cssDel = (e, name) => e.classList.remove(name);
-    sugar.style = (e,n,v) => e.style[n] = v;
-    sugar.styleDel = (e, n) => e.style.removeProperty(n);
+    exports.hasclass = (e, name) => e.classList.contains(name);
+    exports.css = (e,p) => getComputedStyle(e,p);
+    exports.cssrule = (e, rule) => getComputedStyle(e)[rule];
+    exports.cssToggle = (e, rule) => e.classList.toggle(rule);
+    exports.cssReplace = (e, rule, newRule) => e.classList.replace(rule, newRule);
+    exports.cssDel = (e, name) => e.classList.remove(name);
+    exports.style = (e,n,v) => e.style[n] = v;
+    exports.styleDel = (e, n) => e.style.removeProperty(n);
 
-    sugar.height = (e,h) => (typeof h === 'undefined') ? parseFloat(getComputedStyle(e).height.replace("px", "")) : e.style.height = h + 'px';
-    sugar.width = (e,w) =>  (typeof w === 'undefined') ? parseFloat(getComputedStyle(e).width.replace("px", "")) : e.style.width = w + 'px';
-    sugar.offsetheight = (e) => e.offsetHeight;
-    sugar.offsetwidth = (e) => e.offsetWidth;
-    sugar.offsetparent = (e) => e.offsetParent || e;
-    sugar.bounds = (e) => e.getBoundingClientRect();
-    sugar.offset = (e) => flow(
+    exports.height = (e,h) => (typeof h === 'undefined') ? parseFloat(getComputedStyle(e).height.replace("px", "")) : e.style.height = h + 'px';
+    exports.width = (e,w) =>  (typeof w === 'undefined') ? parseFloat(getComputedStyle(e).width.replace("px", "")) : e.style.width = w + 'px';
+    exports.offsetheight = (e) => e.offsetHeight;
+    exports.offsetwidth = (e) => e.offsetWidth;
+    exports.offsetparent = (e) => e.offsetParent || e;
+    exports.bounds = (e) => e.getBoundingClientRect();
+    exports.offset = (e) => flow(
         e.getBoundingClientRect,
         (rect) => { 
              return { 
@@ -103,38 +104,38 @@ var $$ = function() {
             }
         );
 
-    sugar.parent = (e) => e.parentElement;
-    sugar.next = (e) => e.nextElementSibling;
-    sugar.prev = (e) => e.previousElementSibling;
+    exports.parent = (e) => e.parentElement;
+    exports.next = (e) => e.nextElementSibling;
+    exports.prev = (e) => e.previousElementSibling;
 
-    sugar.attr = (e, name) => e.getAttribute(name);
-    sugar.attrDel = (e, name) => e.removeAttribute(name);
-    sugar.attrSet = (e,name,v) => e.setAttribute(e,name,v);
+    exports.attr = (e, name) => e.getAttribute(name);
+    exports.attrDel = (e, name) => e.removeAttribute(name);
+    exports.attrSet = (e,name,v) => e.setAttribute(e,name,v);
 
-    sugar.hide = (e) => e.style.display = 'none';
-    sugar.show = (e) => e.style.display = 'block';
+    exports.hide = (e) => e.style.display = 'none';
+    exports.show = (e) => e.style.display = 'block';
 
     // Events
 
-    sugar.on = (e, name, f) => e.addEventListener(name, f);
-    sugar.off = (e, name, f) => e.removeEventListener(name, f);
-    sugar.click = (e, f) => sugar.on(e, "click", f);
-    sugar.mouseenter = (e, f) => sugar.on(e, "mouseenter", f);
-    sugar.keyup = (e, f) => sugar.on(e, "keyup", f);
-    sugar.keydown = (e, f) => sugar.on(e, "keydown", f);
-    sugar.trigger = (e, name) => (e) ? e.dispatchEvent(new Event(name)) : document.dispatchEvent(new Event(name));
-    sugar.ready = (f) => (document.readyState != "loading") ? f() : document.addEventListener("DOMContentLoaded", f);
+    exports.on = (e, name, f) => e.addEventListener(name, f);
+    exports.off = (e, name, f) => e.removeEventListener(name, f);
+    exports.click = (e, f) => exports.on(e, "click", f);
+    exports.mouseenter = (e, f) => exports.on(e, "mouseenter", f);
+    exports.keyup = (e, f) => exports.on(e, "keyup", f);
+    exports.keydown = (e, f) => exports.on(e, "keydown", f);
+    exports.trigger = (e, name) => (e) ? e.dispatchEvent(new Event(name)) : document.dispatchEvent(new Event(name));
+    exports.ready = (f) => (document.readyState != "loading") ? f() : document.addEventListener("DOMContentLoaded", f);
 
-    sugar.delay = (f) => setTimeout(f, 0);
-    sugar.extend = (t, s) => Object.assign(t, s);
+    exports.delay = (f) => setTimeout(f, 0);
+    exports.extend = (t, s) => Object.assign(t, s);
 
-    sugar.post = (url, data) => fetch(url, {
+    exports.post = (url, data) => fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' }
     }).then(response => response.json());
-    sugar.get = (url) => fetch(url).then(response => response.json());
-    sugar.load = (e, url) => fetch(url)
+    exports.get = (url) => fetch(url).then(response => response.json());
+    exports.load = (e, url) => fetch(url)
         .then(response => response.text())
         .then(text => {
             const parser = new DOMParser();
@@ -145,14 +146,14 @@ var $$ = function() {
         });
 
 
-    sugar.bind = (e) => { 
+    exports.bind = (e) => { 
         if (typeof e === 'string') // passed a selector
-            e = $$.find(e); // find element
+            e = find(e); // find element
 
         var bound = { el: e };
 
-        for (const key in sugar) {
-            var value = sugar[key];
+        for (const key in exports) {
+            var value = exports[key];
             if (typeof value === "function" && CanBindElement(value)) {
                 bound[key] = value.bind(null, e);
             }
@@ -165,7 +166,7 @@ var $$ = function() {
         return e;
     };
 
-    return sugar;
+    return exports;
 
 }();
 
